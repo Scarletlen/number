e6570edab7392a22ede3776fa08853b753fba5db
Scarletlen <dragonzeed1122@gmail.com>
2021-03-22 22:21:53 +0700

    biaddnotlayout

--- src/Component/La.js
+++ src/Component/La.js
@@ -1,7 +1,7 @@
 
 import '../App.css';
 import React from 'react'
-import { Layout, Menu} from 'antd';
+import { Layout, Menu } from 'antd';
 import Bisec from '../lesson/bisection';
 // import Falsepo from './lesson/falseposition';
 // import Onepoint from './lesson/onepoint';
@@ -28,47 +28,48 @@ class La extends React.Component {
     console.log(collapsed);
     this.setState({ collapsed });
   };
-    render()
-    {
-       const { collapsed } = this.state;
-       return (
-            <div>
-                <Layout style={{ minHeight: '100vh' }}>
-                      <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
-                        <div className="logo" />
-                        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                          <SubMenu key="sub1" title="Root of equations">
-                            <Menu.Item key="3">Bisection Method
-                            {/* <Link to='/Bisection'>Bisection</Link>   */}
-                            </Menu.Item>
-                            <Menu.Item key="4">Falseposition Method</Menu.Item>
-                            <Menu.Item key="5">Onepoint Method</Menu.Item>
-                            <Menu.Item key="6">Newton Raphson</Menu.Item>
-                            <Menu.Item key="8">Secant</Menu.Item>
-                          </SubMenu>
-                          <SubMenu key="sub2" title="Martrix">
-                            <Menu.Item key="9">Cramer's Rule</Menu.Item>
-                            <Menu.Item key="10">Gauss Elimination Method</Menu.Item>
-                            <Menu.Item key="11">Gauss Jordan Method</Menu.Item>
-                            <Menu.Item key="8">LU Decomposition Method</Menu.Item>
-                            
-                          </SubMenu>
-                        </Menu>
-                      </Sider>
-                      <Layout className="site-layout">
-                        <Header className="site-layout-background" style={{ padding: 0 }} />
-                        <Content style={{ margin: '0 16px' }}>
-                          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                            {/* <Route path='/Bisection' component={Bisec}></Route> */}
-                          </div>
-                        </Content>
-                        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-                      </Layout>
-                 </Layout>
+  render() {
+    const { collapsed } = this.state;
+    return (
+      <HashRouter>
+        <div>
+          <Layout style={{ minHeight: '100vh' }}>
+            <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
+              <div className="logo" />
+              <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+                <SubMenu key="sub1" title="Root of equations">
+                  <Menu.Item key="3">Bisection Method
+                            {<Link to='/Bisection'>Bisection</Link>}
+                  </Menu.Item>
+                  <Menu.Item key="4">Falseposition Method</Menu.Item>
+                  <Menu.Item key="5">Onepoint Method</Menu.Item>
+                  <Menu.Item key="6">Newton Raphson</Menu.Item>
+                  <Menu.Item key="8">Secant</Menu.Item>
+                </SubMenu>
+                <SubMenu key="sub2" title="Martrix">
+                  <Menu.Item key="9">Cramer's Rule</Menu.Item>
+                  <Menu.Item key="10">Gauss Elimination Method</Menu.Item>
+                  <Menu.Item key="11">Gauss Jordan Method</Menu.Item>
+                  <Menu.Item key="8">LU Decomposition Method</Menu.Item>
 
-            </div>
-       );
-           
-    }
+                </SubMenu>
+              </Menu>
+            </Sider>
+            <Layout className="site-layout">
+              <Header className="site-layout-background" style={{ padding: 0 }} />
+              <Content style={{ margin: '0 16px' }}>
+                <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                  {<Route path='/Bisection' component={Bisec}></Route>}
+                </div>
+              </Content>
+              <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+            </Layout>
+          </Layout>
+
+        </div>
+      </HashRouter>
+    );
+
+  }
 }
 export default La;
