c802e3970551c23f1fe2e95ec474952fb4e1211a
Scarletlen <dragonzeed1122@gmail.com>
2021-04-11 18:55:40 +0700

    addmatrixfromaj

---
+++ src/Component/matrixinput.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Input} from 'antd';
+
+class MatrixInputA extends React.Component{
+
+    createMatrix(){
+        let arrMatrix = []
+        for(let i=0;i<this.props.n;i++){
+            for(let j=0;j<this.props.n;j++){
+                arrMatrix.push(<Input className='matrix-input' name={'inputA_'+i.toString()+'_'+j} placeholder={i.toString()+j} onChange={this.props.onChange}/>)
+            }
+            arrMatrix.push(<div></div>)
+        }
+        return arrMatrix
+    }
+
+    render(){
+        return(
+            <div>
+                {this.createMatrix()}
+            </div>
+        )
+    }
+}
+
+class MatrixInputB extends React.Component{
+
+    createMatrix(){
+        let arrMatrix = []
+        for(let i=0;i<this.props.n;i++){
+            arrMatrix.push(<Input className='matrix-input' name={'inputB_'+i} placeholder={i.toString()} onChange={this.props.onChange}/>)
+            arrMatrix.push(<div></div>)
+        }
+        return arrMatrix
+    }
+
+    render(){
+        return(
+            <div>
+                {this.createMatrix()}
+            </div>
+        )
+    }
+}
+
+export {MatrixInputA, MatrixInputB}
--- src/lesson/cramerru.js
+++ src/lesson/cramerru.js
@@ -1,10 +1,57 @@
 import React from 'react';
+import { Input, Row ,Button,Col  } from 'antd';
+import {MatrixInputA, MatrixInputB} from '../Component/matrixinput'
 class Cremeru extends React.Component{
+    
+    state = 
+    {
+        n: 2,
+        matrixA : [[],[]],
+        matrixB : [],
+    }
+        OnChangeMatrixA = e =>{
+            let changedArr = this.state.matrixA
+            let index = e.target.name.split('_')
+            changedArr[parseInt(index[1])][parseInt(index[2])] = e.target.value
+            console.log(e.target.value)
+            this.setState({matrixA:changedArr})
+        }
+    
+        OnChangeMatrixB = e =>{
+            let changedArr = this.state.matrixB
+            let index = e.target.name.split('_')
+            changedArr[parseInt(index[1])]= e.target.value
+            console.log(e.target.value)
+            this.setState({matrixB:changedArr})
+        }
+    
+        onClickAdd = e =>{
+            if(this.state.n < 6){
+                this.state.matrixA.push([])
+                this.setState({n:this.state.n+1})
+            } 
+        }
+    
+        onClickDel = e =>{
+            if(this.state.n > 2){
+                this.state.matrixA.pop()
+                this.setState({n:this.state.n-1})
+            } 
+        }
     render(){
         return(
-            <div className="cramerruui">
-                
+            <div className ="CramerRule">
+                <h1 className ="ontop"></h1>
+                <Row>
+                    <Col span={22}>
+                        <MatrixInputA n={this.state.n} onChange={this.OnChangeMatrixA} value={this.state.matrixA}/>
+                    </Col>
+                    <Col span={2}>
+                        <MatrixInputB n={this.state.n} onChange={this.OnChangeMatrixB} value={this.state.matrixB}/>
+                    </Col>
+                </Row>
             </div>
+            
         );
 
      
