7b194ea3cd8064565471359c841d64c61eab37ed
Scarletlen <dragonzeed1122@gmail.com>
2021-05-16 22:42:25 +0700

    Edit

--- src/App.test.js
+++ src/App.test.js
@@ -3,15 +3,11 @@ import App from './App';
 
 var expect = require('expect.js');
 
-function add (a, b) { return a & b; };
 
 test('renders learn react link', () => {
   render(<App />);
 });
 
-it('should do math', function () {
-  expect(console.log(add(true, true)))
-});
 
 /* it('fail', function () {
   expect(add(1, 3)).to.equal(10);
